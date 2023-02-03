718ea8eb6030a7df2f0eff9e7e44676fe566058d
000hcl <mnrjohansson@gmail.com>
2023-02-03 16:30:09 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -89,7 +89,8 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = Temporal.PlainDate.from(row.holiday);
-      if (date && date.year === holiday.year) {}
+      if (date &&
+         date.year === holiday.year) {}
     }
   }
 
