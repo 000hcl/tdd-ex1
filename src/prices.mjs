bdcdc08f3ce20bd22f286860f9d2daf3588637aa
000hcl <mnrjohansson@gmail.com>
2023-02-03 16:46:09 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -74,6 +74,7 @@ function createApp(database) {
   }
 
   function dateToPlainDate(date) {
+    return new Temporal.PlainDate(date.getFullYear(), date.getMonth(), date.getDate())
   }
 
   function calculateReduction(date) {
