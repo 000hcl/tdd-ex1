7ebee0be4e28f23c5e71bbf0ed8a8476cf24fc24
000hcl <mnrjohansson@gmail.com>
2023-02-03 16:21:30 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -32,7 +32,7 @@ function createApp(database) {
   function parsePlainDate(dateString) {
     if (dateString) {
   }
-    return Temporal.PlainDate.from(dateString);
+    
   }
 
   function calculateCost(age, type, date, baseCost) {
